adf02f6599853cb21d1c882aa937cb0739207b41
mbobic1 <mbobic1@etf.unsa.ba>
2024-01-02 18:42:49 +0100

    switched from c to c++

---
+++ parallel_dijkstra_c++.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <sys/time.h>
+#include <omp.h>
+#include <vector>
+
+#define N 2048
+#define SOURCE 1
+#define MAXINT 9999999
+
+void dijkstra(int** graph, int source);
+
+int main() {
+    int threads;
+    std::cout << "Please enter number of threads: ";
+    std::cin >> threads;
+    omp_set_num_threads(threads);
+
+    double time_start, time_end;
+    struct timeval tv;
+    struct timezone tz;
+    gettimeofday(&tv, &tz);
+    time_start = (double)tv.tv_sec + (double)tv.tv_usec / 1000000.00;
+
+    int** graph = new int*[N];
+    for (int i = 0; i < N; i++) {
+        graph[i] = new int[N];
+    }
+
+    std::ifstream file("input2048.txt");
+    if (!file) {
+        std::cerr << "Can't open the input file.\n";
+        exit(1);
+    }
+
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            file >> graph[i][j];
+        }
+    }
+
+    dijkstra(graph, SOURCE);
+    gettimeofday(&tv, &tz);
+
+    time_end = (double)tv.tv_sec + (double)tv.tv_usec / 1000000.00;
+    std::cout << "Nodes: " << N << "\n";
+    std::cout << "Time cost is " << time_end - time_start << "\n";
+
+    for (int i = 0; i < N; i++) {
+        delete[] graph[i];
+    }
+    delete[] graph;
+
+    return 0;
+}
+
+void dijkstra(int** graph, int source) {
+    int visited[N];
+    int md;
+    int distance[N]; 
+    int mv;
+    int my_first; 
+    int my_id; 
+    int my_last; 
+    int my_md; 
+    int my_mv; 
+    int my_step; 
+    int nth;
+
+    for (int i = 0; i < N; i++) {
+        visited[i] = 0;
+        distance[i] = graph[source][i];
+    }
+    visited[source] = 1;
+
+    #pragma omp parallel private(my_first, my_id, my_last, my_md, my_mv, my_step) shared(visited, md, distance, mv, nth, graph)
+    {
+        my_id = omp_get_thread_num();
+        nth = omp_get_num_threads();
+        my_first = (my_id * N) / nth;
+        my_last = ((my_id + 1) * N) / nth - 1;
+
+        for (my_step = 1; my_step < N; my_step++) {
+            #pragma omp single
+            {
+                md = MAXINT;
+                mv = -1;
+            }
+
+            my_md = MAXINT;
+            my_mv = -1;
+
+            for (int k = my_first; k <= my_last; k++) {
+                if (!visited[k] && distance[k] < my_md) {
+                    my_md = distance[k];
+                    my_mv = k;
+                }
+            }
+
+            #pragma omp critical
+            {
+                if (my_md < md) {
+                    md = my_md;
+                    mv = my_mv;
+                }
+            }
+
+            #pragma omp barrier
+
+            #pragma omp single
+            {
+                if (mv != -1) {
+                    visited[mv] = 1;
+                }
+            }
+
+            #pragma omp barrier
+
+            if (mv != -1) {
+                for (int j = my_first; j <= my_last; j++) {
+                    if (!visited[j] && graph[mv][j] < MAXINT && distance[mv] + graph[mv][j] < distance[j]) {
+                        distance[j] = distance[mv] + graph[mv][j];
+                    }
+                }
+            }
+
+            #pragma omp barrier
+        }
+    }
+}
